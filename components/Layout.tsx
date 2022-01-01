af6ee4c7aa5f675d12217f65ed8858b1513c72b3
Dhruv Patel <dhruv.codelab@gmail.com>
2022-01-01 02:23:44 -0600

    feat: add Layout component and cleanup the markup

---
+++ components/Layout.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Layout.module.css';
+
+const Layout = ({ children }: { children: React.ReactNode | undefined }) => (
+  <div className={styles.container}>
+    <main className={styles.main}>{children}</main>
+  </div>
+);
+
+export default Layout;
