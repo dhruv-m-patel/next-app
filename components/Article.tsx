36fb9d5bd3e0457a0626e38e88152fd9ae571f9a
Dhruv Patel <dhruv.codelab@gmail.com>
2022-01-01 03:20:42 -0600

    feat: render articles properly with their body and styles

--- components/Article.tsx
+++ components/Article.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Article as ArticleType } from '../types';
+import styles from '../styles/Article.module.css';
 
 interface ArticleProps {
   item: ArticleType;
 }
 
 export default function Article({ item }: ArticleProps) {
-  return <h3>{item.title}</h3>;
+  return (
+    <div className={styles.card}>
+      <h2>{item.title}</h2>
+      <p>{item.body}</p>
+    </div>
+  );
 }
--- components/ArticleList.tsx
+++ components/ArticleList.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { ListOfArticles } from '../types';
 import Article from './Article';
+import styles from '../styles/ArticleList.module.css';
 
 interface ArticleListProps {
   articles: ListOfArticles;
 }
 export default function ArticleList({ articles }: ArticleListProps) {
-  return articles.map((item) => <Article key={item.id} item={item} />);
+  return (
+    <div className={styles.grid}>
+      {articles.map((item) => (
+        <Article key={item.id} item={item} />
+      ))}
+    </div>
+  );
 }
